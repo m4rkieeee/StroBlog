88ddbcee90dc32be782270b5e7324175bc913e07
m4rkieeee <m.dejonge04@student.alfa-college.nl>
2022-11-14 15:27:46 +0100

    Made migrations, controllers, models.
    Added migrations, controllers, models, added lorem ipsum to homepage to see connection between these. Added CSS to app.layout to load in.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,14 @@
 @extends('layouts.app')
-
+@section('title')
+    Home
+@endsection
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+    @foreach($post as $posts)
+<article>
+    <h1> {{ $posts->title }} </h1>
+    <p>Posted by <b>{{ $posts->user->name }}</b> on <b>{{ $posts->created_at->format('d F Y, H:i') }}</b></p>
+    <hr>
+    <p> {{ $posts->text }}</p>
+</article>
+@endforeach
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,11 +3,11 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <link href="{{ asset('css/custom.css') }}" rel="stylesheet">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>@yield('title') - Blog</title>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
